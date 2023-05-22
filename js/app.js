167016dba577d3a52871fab191cde813d0909812
LegiumProf <107118176+LegiumProf@users.noreply.github.com>
2023-05-23 01:05:14 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#cssmenu li.has-sub > a').on('click', function(){
+        $(this).removeAttr('href');
+        var element = $(this).parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
+            element.find('li').removeClass('open');
+            element.find('ul').slideUp();
+        }
+        else {
+            element.addClass('open');
+            element.children('ul').slideDown();
+            element.siblings('li').children('ul').slideUp();
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp();
+        }
+    });
+ 
+    $('#cssmenu>ul>li.has-sub>a').append('<span class="holder"></span>');
+});
+$(".box .block").show();
+$(".closed .block").hide();
+$("div.hide").click(function() {
+  $(this).toggleClass("show").next().slideToggle("medium");
+});
+
---
+++ js/login.js
@@ -0,0 +1,19 @@
+#login
+{
+    position: absolute;
+    z-index: 0;
+}
+#login:before
+{
+    content: '';
+    position: absolute;
+    z-index: -1;
+    border: 1px dashed #ccc;
+    top: 5px;
+    bottom: 5px;
+    left: 5px;
+    right: 5px;
+    -moz-box-shadow: 0 0 0 1px #fff;
+    -webkit-box-shadow: 0 0 0 1px #fff;
+    box-shadow: 0 0 0 1px #fff;
+}
